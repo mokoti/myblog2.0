0e700bf340cb159c6bb4a7f0be23522338589db6
mokoti <m.holy.boc@gmail.com>
2019-02-17 22:42:18 +0900

    コメント追加

--- app/Post.php
+++ app/Post.php
@@ -8,15 +8,14 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    // 論理削除を行う
     use SoftDeletes;
 
-
-	// Mass Assignment
-	protected $fillable = ['title','body','category_id',];
+	// Postテーブルに用いるデータ
+    protected $fillable = ['title','body','category_id',];
     protected $dates = ['deleted_at'];
 
-
-	// Validate Rule
+	// バリデーション
     public static function getValidateRule(Post $post=null){
         if($post){
             $ignore_unique = $post->id;
@@ -25,40 +24,46 @@ class Post extends Model
         }
         $table_name = 'posts';
         $validation_rule = [
-
             'model.title' => 'required',
             'model.body' => 'nullable',
             'model.category_id' => 'integer|nullable',
-
         	'pivots.tag.*.priority' => 'integer|nullable',
-
         ];
+        // これは何？
         if($post){
-
         }
         return $validation_rule;
     }
 
+    // ブログのポストのコメントを取得
 	public function comments() {
+        // 1(post)対多(comments)
+        // Commentモデルの外部キーは自動的にpost_idになる
 		return $this->hasMany('App\Comment');
 	}
 
-
+    // カテゴリーを取得
 	public function category() {
+        // １(category)対多(posts)(Inverse)
 		return $this->belongsTo('App\Category');
 	}
 
-
+    // タグを取得
 	public function tags() {
+        // 多対多
 		return $this->belongsToMany('App\Tag')
+        // 中間テーブル
 		->withPivot('priority')
 		->orderBy('id')
 		->withTimestamps();
 	}
 
-
+    // 指定している３つのキーの値をlistで返す
 	public static function getLists() {
 		$lists = [];
+        // 指定したキーの全コレクションを取得、
+        // なのでモデルとして生成されている下記３つの全ての値をを取得する(どうなんだこれ・・・)
+        // おそらくモデルであらかじめ取得する処理を前提にしているのでDBへの接続はここではないはず
 		$lists['Category'] = Category::pluck( 'name' ,'id' );
 		$lists['Comment'] = Comment::pluck( 'title' ,'id' );
 		$lists['Tag'] = Tag::pluck( 'name' ,'id' );
